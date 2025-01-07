26493081914e610738021aa6b5b398d7d6b62b4e
Anshul Rai <anshulrai02092003@gmail.com>
2025-01-07 13:38:29 +0530

    add substract feature

--- Calculator.java
+++ Calculator.java
@@ -16,6 +16,9 @@ public class Calculator {
             case '+':
                 result = num1 + num2;
                 break;
+	    case '-':
+                result = num1 - num2;
+                break;
 	    default:
                 valid = false;
         }
